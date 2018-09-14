50f6e58e3abd85daebbc68a539008bbe04feee3b
Xavier Carrel <xavier.carrel@cpnv.ch>
2018-09-14 08:54:25 +0200

    First

---
+++ Classroom/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Classroom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdDessiner = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdDessiner
+            // 
+            this.cmdDessiner.Location = new System.Drawing.Point(525, 2);
+            this.cmdDessiner.Name = "cmdDessiner";
+            this.cmdDessiner.Size = new System.Drawing.Size(99, 23);
+            this.cmdDessiner.TabIndex = 11;
+            this.cmdDessiner.Text = "Dessiner";
+            this.cmdDessiner.UseVisualStyleBackColor = true;
+            this.cmdDessiner.Click += new System.EventHandler(this.cmdDessiner_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 462);
+            this.Controls.Add(this.cmdDessiner);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cmdDessiner;
+    }
+}
+
---
+++ Classroom/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Classroom
+{
+    public partial class Form1 : Form
+    {
+        Graphics G;
+
+        Sylvain theSylvain = new Sylvain(10, 10);
+        Jimmy theJimmy = new Jimmy(50, 10);
+        Dylan theDylan = new Dylan(90, 10);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(MyPaint);
+        }
+
+        private void MyPaint(object sender, PaintEventArgs e)
+        {
+            G = e.Graphics;
+            theSylvain.Draw(G);
+            theJimmy.Draw(G);
+            theDylan.Draw(G);
+        }
+
+        private void cmdDessiner_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            this.Refresh();
+            Cursor = Cursors.Arrow;
+        }
+
+    }
+}
