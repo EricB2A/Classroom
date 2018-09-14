1d1e11f1b6dea1ccb511da884d3a810c82c83501
IlanRuiz <42832268+IlanRuiz@users.noreply.github.com>
2018-09-14 10:17:06 +0200

    Classe de Ilan

---
+++ Classroom/Ilan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Ilan
+    {
+
+        public float height;
+        public int age;
+        public string eye_color;
+        public string hair_color;
+        public float weight;
+        List<string> sports;
+        List<string> hobbys;
+        public string work;
+        public DateTime birthday;
+        public bool righthanded;
+        public string favorite_song;
+        public string favorite_movie;
+        public string favorite_game;
+        public string gender;
+        public string dream;
+        public string town;
+        public string feeling;
+        
+
+        public string draw() {
+            return;
+        }
+
+        public Ilan()
+        {
+
+            sports = new List<string>();
+            hobbys = new List<string>();
+
+        }
+    }
+}
