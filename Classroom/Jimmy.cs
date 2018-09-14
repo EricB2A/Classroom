f5e482bac6aca6c04d76b01ce9750bacbe401357
JimmyCatarino <36159176+JimmyCatarino@users.noreply.github.com>
2018-09-14 14:33:31 +0200

    Add files via upload

--- Classroom/Jimmy.cs
+++ Classroom/Jimmy.cs
@@ -22,26 +22,46 @@ namespace Classroom
         public float Height;
         public DateTime Birthday;
         private bool Gender;
+        public bool GoodHumor;
 
         //Constructor
         public Jimmy(int x, int y)
         {
             px = x;
             py = y;
+            GoodHumor = true;
         }
 
         //Method
         public void Draw(Graphics G)
         {
-            String drawString = "JCD";
+            String drawString = "Jimmy Catarino";
             Font drawFont = new Font("Arial", 10);
             SolidBrush drawBrush = new SolidBrush(Color.Black);
 
-            Image newImage = Image.FromFile("1.PNG");
+            Image newImage = Image.FromFile("jimmy.PNG");
             Pen P = new Pen(Color.Red);
             G.DrawImage(newImage, px, py);
-            G.DrawString(drawString, drawFont, drawBrush, px, py);
+            if(GoodHumor == true)
+            {
+                G.DrawString(drawString, drawFont, drawBrush, px, py+70);
+            }
+
         }
+
+        public void DoSomething()
+        {
+            //System.Windows.Forms.MessageBox.Show("Pour Quentin ^^ : \r \rJe m'présente, je m'appelle Henri J'voudrais bien réussir ma vie, être aimé Etre beau gagner de l'argent Puis surtout être intelligent Mais pour tout ça il faudrait que j'bosse à plein temps J'suis chanteur, je chante pour mes copains ");
+            if(GoodHumor == false)
+            {
+                GoodHumor = true;
+            }
+            else
+            {
+                GoodHumor = false;
+            }
+        }
+
         public void Play() {}
         public void GoOut() {}
         public void Sleep() {}
