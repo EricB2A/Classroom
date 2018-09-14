03e4b9c38ffca62343b5fb5f86bec96049222fd2
FilipeFerreiraDantas <42832301+FilipeFerreiraDantas@users.noreply.github.com>
2018-09-14 13:46:38 +0200

    Update Filipe.cs

--- Classroom/Filipe.cs
+++ Classroom/Filipe.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace Classroom
 {
@@ -11,7 +13,6 @@ namespace Classroom
     {
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
-        public DateTime Birth { get; private set; }
         public string Gender { get; private set; }
         public string HairColor { get; private set; }
         public string EyesColor { get; private set; }
@@ -19,16 +20,59 @@ namespace Classroom
         public int Heigth { get; private set; }
         public float Weigth { get; private set; }
 
-        public Filipe()
+        public int x;
+        public int y;
+
+        public Filipe(int p1, int p2)
         {
+            x = p1;
+            y = p2;
+
+            FirstName = "Filipe";
+            LastName = "LastName";
+            Gender = "Male";
+            HairColor = "Brown";
+            EyesColor = "Brown";
+            SkinColor = "White";
+            Heigth = 76;
+            Weigth = 50;
 
         }
 
         public void Draw(Graphics G)
         {
-            Pen P = new Pen(Color.DarkRed);
-            //G.DrawPolygon(P);
+
+            Pen pen = new Pen(Color.DarkViolet);
+
+            G.DrawLine(pen, x, y, x+125/2, y); //x
+            G.DrawLine(pen, x, y, x, y+20/2); //x
+            G.DrawLine(pen, x, y+20/2, x + 125/2, y+20/2); //x
+            G.DrawLine(pen, x+125/2, y, x + 125/2, y + 20/2); //x
+            G.DrawLine(pen, x+50/2, y-50/2, x+50/2, y+125/2); //y
+            G.DrawLine(pen, x+70/2, y-50/2, x+50/2 , y-50/2); //x
+            G.DrawLine(pen, x + 70/2, y - 50/2, x + 70/2, y + 125/2); //y
+            G.DrawLine(pen, x+50/2, y+125/2, x+70/2 , y+125/2); //x
+
+            // Create string to draw.
+            String drawString = "Jesus";
+
+            // Create font and brush.
+            Font drawFont = new Font("Arial", 16);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            // Create point for upper-left corner of drawing.
+            x = x + 63;
+            y = y - 50;
+
+            // Set format of string.
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+
+            // Draw string to screen.
+            G.DrawString(drawString, drawFont, drawBrush, x, y, drawFormat);
         }
+
+       
 
     }
 }
