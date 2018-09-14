07b1ec4f07ed4807673d37717053e2d3c6840960
Cyril Goldenschue <Cyril.Goldenschue@cpnv.ch>
2018-09-14 14:24:03 +0200

    Classe Cyril

--- Classroom/Cyril.cs
+++ Classroom/Cyril.cs
@@ -13,8 +13,6 @@ namespace Classroom
     {
         public int Spx;
         public int Spy;
-        public int SpyF;
-        public int SpxF;
 
         public string Name;
         public Color SkinColor;
@@ -45,80 +43,46 @@ namespace Classroom
                 "Jeux videos",
                 "Lecture"
             };
-            Spy = py + 50;
-            Spx = px + 100;
-            SpxF = 100 + px;
-            SpyF = 50 +py;
-        }
-
-
+            Spx = px;
+            Spy = py;
+            
+    }
         public void Draw(Graphics G)
         {
             Pen P = new Pen(Color.Black);
             Pen pen = new Pen(Color.White);
             // Corps
-            G.DrawLine(P, Spx + 200, Spy + 150, SpxF + 200, SpyF + 300);
+            G.DrawLine(P, Spx + 100, Spy + 40, Spx + 100, Spy + 120);
             //Pied droite
-            G.DrawLine(P, Spx + 200, Spy + 300, SpxF + 150, SpyF + 380);
+            G.DrawLine(P, Spx + 100, Spy + 120, Spx + 80, Spy + 140);
             //Pied gauche
-            G.DrawLine(P, Spx + 200, Spy + 300, SpxF + 250, SpyF + 380);
+            G.DrawLine(P, Spx + 100, Spy + 120, Spx + 120, Spy + 140);
             //Tête
-            G.DrawEllipse(P, Spx + 165, Spy + 80, SpxF - 40, SpyF + 10);
+            G.DrawEllipse(P, Spx + 85, Spy + 10, 30, 30);
             /*//Bouche
             Rectangle rect = new Rectangle(Spx + 175, Spy + 90, Spx - 50, Spy);
             float startAngle = 35.0F;
             float sweepAngle = 100.0F;
             G.DrawArc(P, rect, startAngle, sweepAngle);*/
             //Yeux
-            G.DrawEllipse(P, Spx + 180, Spy + 100, SpxF - 100, SpyF - 50);
-            G.DrawEllipse(P, Spx + 210, Spy + 100, SpxF - 100, SpyF - 50);
+            G.DrawEllipse(P, Spx + 90, Spy + 20, 5, 5);
+            G.DrawEllipse(P, Spx + 105, Spy + 20, 5, 5);
             SolidBrush brush = new SolidBrush(EyeColor);
 
-            G.FillEllipse(brush, Spx + 180, Spy + 100, SpxF - 100, SpyF - 50);
-            G.FillEllipse(brush, Spx + 210, Spy + 100, SpxF - 100, SpyF - 50);
-            int pz = Spy + 150;
-            /*int pw = py + 148;
-            int i = 0;
-            int x = 0;
-            bool Vrai = true;
-            while (Vrai)
-            {
-                if(x == 0)
-                {
-                    i++;
-                    if(i >= 100)
-                    {
-                        x = 1;
-                    }
-                }
-                if(x == 1)
-                {
-                    i--;
-                    if(i == 0)
-                    {
-                        x = 0;
-                        //Vrai = false;
-                    }
-                }
-                */
-                //Bras droite
-                G.DrawLine(P, Spx + 200, Spy + 200, SpxF + 150, pz);
-                //Bras droite
-                //G.DrawLine(pen, px + 200, py + 200, px + 150, pw + i);
-                //Bras gauche
-                G.DrawLine(P, Spx + 200, Spy + 200, SpxF + 250, pz);
-                
-                //Bras gauche
-                //G.DrawLine(pen, px + 200, py + 200, px + 250, pw + i);
-               /* Thread.Sleep(5);
-            }*/
+            G.FillEllipse(brush, Spx + 90, Spy + 20, 5, 5);
+            G.FillEllipse(brush, Spx + 105, Spy + 20, 5, 5);
 
-            
+            //Bras droite
+            G.DrawLine(P, Spx + 100, Spy + 50, Spx + 80, Spy + 30);
+            //Bras gauche
+            G.DrawLine(P, Spx + 100, Spy + 50, Spx + 120, Spy + 30);
+
+
+
+
 
 
 
         }
     }
-
-
 }
