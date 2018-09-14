bd4f3d0dcb8083cb8a7cb6549a51df53f5ca6f12
SylvainGandini <36150399+SylvainGandini@users.noreply.github.com>
2018-09-14 14:53:59 +0200

    Add files via upload

--- Classroom/sylvain.cs
+++ Classroom/sylvain.cs
@@ -11,20 +11,39 @@ namespace Classroom
         //Properties
         public int X;
         public int Y;
-
+        public bool t;
         public Sylvain(int rX, int rY)
         {
             X = rX;
             Y = rY;
+            t = false;
         }
         //Method
         public void Draw(Graphics G)
         {
-            Font drawfont = new Font("Arial", 10);
-            SolidBrush drawbrush = new SolidBrush(Color.Black);
+            if (t == true)
+            {
+                Font drawfont = new Font("Arial", 10);
+                SolidBrush drawbrush = new SolidBrush(Color.Black);
+                G.DrawString("Sylvain Gandini", drawfont, drawbrush, X, Y + 100);
+            }
+            else
+            {
+                Font drawfont = new Font("Arial", 10);
+                SolidBrush drawbrush = new SolidBrush(Color.Black);
+                G.DrawString("Délégué", drawfont, drawbrush, X, Y + 100);
+            }
             Image newImage = Image.FromFile( "sylvain_gandini.bmp");
             G.DrawImage(newImage, X, Y);
-            G.DrawString("Sylvain Gandini",drawfont, drawbrush, X,Y+100);
+           
+        }
+        public void DoSomething()
+        {
+            if (t == false)
+            {
+                t = true;
+            }
+            else t = false;
         }
 
     }
